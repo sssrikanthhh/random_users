0646c69cb408f6c194ee1d677cbf0be6137e821d
srikanth <chiramanasrikanth265@gmail.com>
2022-07-14 09:45:07 +0530

    ADD: Create a User component
    - Create a User component which will display all the user
      details.
    If applied this commit will create a User component.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,5 +1,29 @@
+import './User.css';
+
 const User = ({ user }) => {
-	return <div>{user.name.first}</div>;
+	console.log(user);
+	return (
+		<div className='user_card'>
+			<img src={user.picture.large} alt='' />
+			<div className='user_card_details'>
+				<h2>
+					Name: {user.name.title}. {user.name.first} {user.name.last}
+				</h2>
+				<p>
+					<strong>Email:</strong> {user.email}
+				</p>
+				<p>
+					<strong>Gender:</strong> {user.gender}
+				</p>
+				<p>
+					<strong>Age:</strong> {user.dob.age}
+				</p>
+				<p>
+					<strong>Country:</strong> {user.location.country}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default User;
